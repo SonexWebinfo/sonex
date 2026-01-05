966db84da33096d43407ae4618790b68eaac7f8f
Sonex Webinfo <sonexwebinfo@gmail.com>
2026-01-05 23:41:52 +0530

    about 2016

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,18 +5,19 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "About Page | Free Next.js Template for Startup and SaaS",
-  description: "This is About Page for Startup Nextjs Template",
-  // other metadata
+  title: "About Sonex Infotech | Innovative IT Solutions Company",
+  description:
+    "Founded in 2016, Sonex Infotech is a technology-driven IT company delivering scalable, secure, and innovative software solutions for startups, enterprises, and growing businesses.",
 };
 
 const AboutPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="About Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="About Us"
+        description="Founded in 2016, Sonex Infotech has been delivering innovative and reliable IT solutions, helping startups and enterprises transform ideas into powerful digital products using modern technology."
       />
+
       <AboutSectionOne />
       <AboutSectionTwo />
     </>
