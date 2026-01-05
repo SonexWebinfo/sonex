a2c6b2b165cab7b470d10ba561682f9ca688b94a
Sonex Webinfo <sonexwebinfo@gmail.com>
2026-01-05 23:04:41 +0530

    button remove

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -17,20 +17,7 @@ const Hero = () => {
                 <p className="mb-12 text-base leading-relaxed! text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
                   Sonex Infotech is a technology-driven software company delivering scalable, secure, and innovative digital solutions. We specialize in web & mobile development, enterprise systems, and cloud-based platforms that help businesses grow, automate operations, and stay competitive in a rapidly evolving digital world.
                 </p>
-                <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-                  <Link
-                    href="https://nextjstemplates.com/templates/saas-starter-startup"
-                    className="rounded-xs bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
-                  >
-                    🔥 Get Pro
-                  </Link>
-                  <Link
-                    href="https://github.com/NextJSTemplates/startup-nextjs"
-                    className="inline-block rounded-xs bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
-                  >
-                    Star on GitHub
-                  </Link>
-                </div>
+
               </div>
             </div>
           </div>
