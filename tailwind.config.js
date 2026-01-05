da2d7738be8276af96f564d01e15da83ec750739
Sonex Webinfo <sonexwebinfo@gmail.com>
2026-01-06 00:08:55 +0530

    country history check

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,36 @@ module.exports = {
     ],
     theme: {
         extend: {
+            /* =========================
+               Animations
+            ========================= */
             animation: {
                 scroll: "scroll 30s linear infinite",
+                blob: "blob 30s infinite",
             },
+
+            /* =========================
+               Keyframes
+            ========================= */
             keyframes: {
                 scroll: {
                     "0%": { transform: "translateX(0)" },
                     "100%": { transform: "translateX(-50%)" },
+                },
+
+                blob: {
+                    "0%": {
+                        transform: "translate(0px, 0px) scale(1)",
+                    },
+                    "33%": {
+                        transform: "translate(40px, -60px) scale(1.1)",
+                    },
+                    "66%": {
+                        transform: "translate(-30px, 30px) scale(0.95)",
+                    },
+                    "100%": {
+                        transform: "translate(0px, 0px) scale(1)",
+                    },
                 },
             },
         },
