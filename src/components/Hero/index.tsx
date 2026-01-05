a42efd872585a209574c69f61e7814b6219137db
Sonex Webinfo <sonexwebinfo@gmail.com>
2026-01-05 23:03:31 +0530

    title and description change

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,13 +12,10 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Sonex Infotech
+                  Sonex Infotech – Your Trusted Partner for Innovative IT Solutions
                 </h1>
                 <p className="mb-12 text-base leading-relaxed! text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Startup is free Next.js template for startups and SaaS
-                  business websites comes with all the essential pages,
-                  components, and sections you need to launch a complete
-                  business website, built-with Next 16.x and Tailwind CSS.
+                  Sonex Infotech is a technology-driven software company delivering scalable, secure, and innovative digital solutions. We specialize in web & mobile development, enterprise systems, and cloud-based platforms that help businesses grow, automate operations, and stay competitive in a rapidly evolving digital world.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
